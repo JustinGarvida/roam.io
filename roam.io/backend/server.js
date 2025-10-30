ad0cdb3e5db0cfc64acb5706140d0c76d97c9126
pingaurayt <pingaurayt@gmail.com>
2025-10-29 23:42:54 -0400

    Change project structure. Split backend and frontend. Basic backend server

---
+++ roam.io/backend/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+let { Pool } = pg;
+
+let app = express();
+
+//Need cors if frontend and backend are on different ports
+app.use(cors());
+app.use(express.json());
+let pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+app.get("/api/login", (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  console.log("HI");
+});
+
+let PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
